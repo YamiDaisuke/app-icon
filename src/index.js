541abbe6022d107febfe0b40dd4ea229c2c5ad3b
YamiDaisuke <daisuke.sysoft@gmail.com>
2018-03-13 11:09:40 -0300

    Turn return into a promise

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 'use strict';
 
+const chalk = require('chalk');
+
 const isImagemagickInstalled = require('../src/imagemagick/is-imagemagick-installed');
 const generate = require('../src/generate');
 const labelImage = require('../src/label/label-image');
 const fileExists = require('../src/utils/file-exists');
 
 module.exports = ({iconPath, searchPath, platforms}) => {
-  isImagemagickInstalled()
+
+  return new Promise((resolve, reject) => {
+    isImagemagickInstalled()
       .catch((err) => { throw err; })
       .then((imageMagickInstalled) => {
         if (!imageMagickInstalled) {
           console.error('  Error: ImageMagick must be installed. Try:');
           console.error('    brew install imagemagick');
-          return process.exit(1);
+          return reject(new Error('ImageMagick must be installed'));
         }
 
         //  Check that we have a source icon.
@@ -21,13 +25,17 @@ module.exports = ({iconPath, searchPath, platforms}) => {
       .then((exists) => {
         if (!exists) {
           console.error(`Source file '${icon}' does not exist. Add the file or specify source icon with the '--icon' parameter.`);
-          return process.exit(1);
+          return reject('Icon source file does not exist');
         }
         //  Generate some icons.
         return generate({ sourceIcon: icon, search, platforms });
       })
+      .then(() => {
+        resolve();
+      })
       .catch((generateErr) => {
         console.error(chalk.red(`An error occurred generating the icons: ${generateErr.message}`));
-        return process.exit(1);
+        return reject(generateErr);
       });
+  });
 };
